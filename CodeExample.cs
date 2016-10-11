b41c1c1014e97201e6d0f368b69dac7061a3c1c7
Marco Mignano <marco.mignano89@gmail.com>
2016-10-11 08:30:27 +0200

    Added example file

---
+++ CodeExample.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System;
+
+public class SpriteSheet : MonoBehaviour {
+
+    #region FIELDS
+    //Exsposing private field to the Unity editor
+    [SerializeField]
+    private GameObject canvas;
+    //Used for the lazy instantiations
+    private string password;
+    
+    #endregion
+
+    #region PROPERTIES
+    //Simple Properties creation
+    public int Foo { get; set; }
+    //Exsposing private field used by the editor example
+    public GameObject Canvas { get {return this.canvas;} }
+    //Lazy instantiation example
+    private string Password
+    {
+        get
+        {
+            if (this.password == null)
+            { this.password = this.GeneratePassword(); }
+            return this.password;
+        }
+    }
+    #endregion
+
+    #region MONOBHEAVIOR
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    #endregion
+
+    #region PUBLIC METHODS
+    //Insert here your pubblic methods
+    #endregion
+
+    #region PRIVATE METHODS
+    private string GeneratePassword()
+    {
+        return Constants.Secrets.password;
+    }
+    #endregion
+}
